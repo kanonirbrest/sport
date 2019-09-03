79692b0c45d436390786fb9d92c880ce39653e7b
Sakovich <yasakovich@scnsoft.com>
2019-09-03 15:45:17 +0300

    add | add base grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './common/components/Header/Header';
+import Footer from './common/components/Footer/Footer';
+import MainContent from './components/MainContent/MainContent';
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      <Sidebar/>
+      <MainContent/>
+      <Footer/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +26,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import s from './MainContent.module.css';
+
+export default class MainContent extends Component {
+    render() {
+        return (
+            <main>
+                main
+            </main>
+        )
+    }
+}
