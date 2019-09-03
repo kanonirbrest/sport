5629effc75cab19d8604cd64ce0e02fdcfe3a2a4
Sakovich <yasakovich@scnsoft.com>
2019-09-03 17:24:35 +0300

    add | add mock articles layout

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -5,7 +5,42 @@ export default class MainContent extends Component {
     render() {
         return (
             <main>
-                main
+                <div className={s.postsWrapper}>
+                    <div className={s.post1}>
+                        <img src="" alt=""/>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post2}>
+                        <img src="" alt=""/>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post3}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post4}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post5}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post6}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post7}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                    <div className={s.post8}>
+                        <div className={s.title}>mytitle</div>
+                        <div className={s.text}>description</div>
+                    </div>
+                </div>
             </main>
         )
     }
