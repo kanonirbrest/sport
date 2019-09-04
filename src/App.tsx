ae863131d65bd66b4f00a19a135c3a7a74a1908c
Sakovich <yasakovich@scnsoft.com>
2019-09-04 15:12:10 +0300

    refactor | refactor styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './common/components/Header/Header';
-import Footer from './common/components/Footer/Footer';
+// import Footer from './common/components/Footer/Footer';
 import MainContent from './components/MainContent/MainContent';
 import Sidebar from './components/Sidebar/Sidebar';
 import Banner from './components/Banner/Banner';
+
+import './assets/styles/mainPage.css';
+import './assets/styles/paperEffect.css';
 
 const App: React.FC = () => {
   return (
@@ -18,13 +21,6 @@ const App: React.FC = () => {
         <MainContent/>
       </div>
       </div>
-      {/* <div className="App effect2 box">
-        <Header/>
-        <Sidebar/>
-        <Banner/>
-        <MainContent/>
-      </div> */}
-      {/* <Footer/> */}
     </div>
   );
 }
--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Menu from './../../../components/Menu/Menu';
+import s from './Header.module.css';
 
 export default function Header() {
     return (
         <header>
-            <div className="headerContainer">
+            <div className={s.headerContainer}>
                 <h1>Газета.by</h1>
                 <h2>Брестское Динамо в серии пенальти выигрывает кубок беларуси 2007!</h2>
             </div>
