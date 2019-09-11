5049f4a5a0c8f1fb88833fc375612cf49a2a5489
Sakovich <yasakovich@scnsoft.com>
2019-09-11 17:02:37 +0300

    add navigations

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Menu from './../../../components/Menu/Menu';
 import s from './Header.module.css';
+import { NavLink, Link  } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header>
             <div className={s.headerContainer}>
-                <h1>Газета.by</h1>
+                <Link to='/' style={{ textDecoration: 'none', color: 'black' }}> <h1>Газета.by</h1> </Link>
                 <h2>Брестское Динамо в серии пенальти выигрывает кубок беларуси 2007!</h2>
             </div>
             <Menu/>
